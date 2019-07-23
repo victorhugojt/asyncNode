fd66b6b896bc91a174e6356ee9763a1ef096474a
Victor Hugo Jimenez Torres <vjimenezt@psl.com.co>
2019-07-23 06:09:37 -0500

    parser

--- dist/src/parser.js
+++ dist/src/parser.js
@@ -1,11 +1,37 @@
+const compare = (actual, stackElement) => {
+    switch (actual) {
+        case ']':
+            if (stackElement !== '[')
+                return false;
+            break;
+        case '}':
+            if (stackElement !== '{')
+                return false;
+            break;
+        case ')':
+            if (stackElement !== '(')
+                return false;
+            break;
+        default: break;
+    }
+};
+const isOpenCurly = (curly) => curly === '[' || curly === '{' || curly === '(' ? true : false;
 const isValid = (str) => {
     const stack = [];
     str.split('').forEach((element) => {
-        if (element === '[' || element === '{' || element === '(') {
+        if (isOpenCurly(element)) {
             stack.push(element);
         }
+        else {
+            if (stack.length === 0)
+                return false;
+            compare(element, stack.pop());
+        }
     });
-    console.log(stack);
+    if (stack.length === 0)
+        return true;
+    return false;
 };
-isValid('[{[]}]');
+// console.log(isValid('[{[]}]'));
+console.log(isValid('[]{}'));
 //# sourceMappingURL=parser.js.map
--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,29 @@
 const compare = (actual: string, stackElement: string) => {
   switch (actual) {
-    case ')' : console.log(actual);
+    case ']' : if (stackElement !== '[') return false; break;
+    case '}' : if (stackElement !== '{') return false; break;
+    case ')' : if (stackElement !== '(') return false; break;
+    default : break;
   }
 }
+
+const isOpenCurly = (curly: string): boolean => 
+  curly === '[' || curly === '{' || curly === '(' ? true : false;
 
 const isValid = (str: string) => {
   const stack: string[] = [];
   str.split('').forEach( (element: string) => {
-    if (element === '[' || element === '{' || element === '(') {
+    if (isOpenCurly(element)) {
       stack.push(element);
     } else {
+      if (stack.length === 0) return false;
       compare(element, stack.pop());
     } 
   });
-  console.log(stack);
+  if (stack.length === 0) return true;
+  return false;
 }
 
-isValid('[{[]}]');
+console.log(isValid('[{[]}]'));
+
+// console.log(isValid('[]{}'));
